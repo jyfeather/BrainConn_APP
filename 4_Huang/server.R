d03c7eef51e34ebc3b1796d30cd6e11750c64994
Yan Jin <jyfeather@gmail.com>
2015-09-15 23:03:20 -0700

    add Dr. Huang's example

---
+++ 4_Huang/server.R
@@ -0,0 +1,64 @@
+library(glasso)
+library(d3heatmap)
+
+# function definition
+matScale <- function(mat, diag = FALSE) {
+  mat <- abs(mat)
+  if (!diag) {
+    diag(mat) <- 0
+  }
+  mat <- apply(mat, MARGIN = 2, FUN = function(x) {(x - min(x))/diff(range(x))})
+  return(mat)
+}
+
+# for d3heatmap use
+reorderfun <- function(d, w) {
+  return(d)
+}
+
+# get dendrogram for reorder
+getDendrogram <- function(mat) {
+  distance <- dist(mat)
+  cluster <- hclust(distance, method = "ward.D")
+  dendrogram <- as.dendrogram(cluster)
+  Rowv <- rowMeans(mat)
+  dendrogram <- reorder(dendrogram, Rowv)
+  return(dendrogram)
+}
+
+# Load data
+dataobs_AD <- read.csv(file = "MRI_AD.csv", header = T)
+dataobs_NL <- read.csv(file = "MRI_NL.csv", header = T)
+dataobs_MCI <- read.csv(file = "MRI_MCI.csv", header = T)
+nodes <- read.csv(file = "AAL_Yan.csv")
+dataobs_AD <- dataobs_AD[sort(nodes$id),]
+dataobs_NL <- dataobs_NL[sort(nodes$id),]
+dataobs_MCI <- dataobs_MCI[sort(nodes$id),]
+
+# glasso
+cov_AD <- cov(t(dataobs_AD))
+cov_NL <- cov(t(dataobs_NL))
+cov_MCI <- cov(t(dataobs_MCI))
+
+shinyServer(function(input, output) {
+  # heatmap plot
+  output$heat_AD<- renderD3heatmap({
+    fit_AD <- glasso(s = cov_AD, rho = input$lambda)
+    gh_AD <- matScale(fit_AD$wi)
+    dendrogram <- getDendrogram(gh_AD)
+    #heatmap(gh_AD, Rowv = dendrogram, Colv = "Rowv", reorderfun = reorderfun) 
+    d3heatmap(gh_AD, Rowv = dendrogram, Colv = "Rowv", reorderfun = reorderfun, colors = heat.colors(100)) 
+  })
+  output$heat_NL<- renderD3heatmap({
+    fit_NL <- glasso(s = cov_NL, rho = input$lambda)
+    gh_NL <- matScale(fit_NL$wi)
+    dendrogram <- getDendrogram(gh_NL)
+    d3heatmap(gh_NL, Rowv = dendrogram, Colv = "Rowv", reorderfun = reorderfun, colors = heat.colors(100)) 
+  })
+  output$heat_MCI<- renderD3heatmap({
+    fit_MCI <- glasso(s = cov_MCI, rho = input$lambda)
+    gh_MCI <- matScale(fit_MCI$wi)
+    dendrogram <- getDendrogram(gh_MCI)
+    d3heatmap(gh_MCI, Rowv = dendrogram, Colv = "Rowv", reorderfun = reorderfun, colors = heat.colors(100)) 
+  })
+})
---
+++ 4_Huang/ui.R
@@ -0,0 +1,23 @@
+library(shiny)
+library(d3heatmap)
+
+shinyUI(fluidPage(
+  titlePanel('Brain Connectivity Network Simulator'),
+  tags$h3('Dr. Shuai Huang\'s work'),
+  hr(),
+  
+  navlistPanel(
+    "Compare NL, MCI and AD",
+    tabPanel("Matrix View",
+             tabsetPanel(
+               tabPanel("NL", d3heatmapOutput("heat_NL", width = "1000px", height = "800px")),
+               tabPanel("MCI", d3heatmapOutput("heat_MCI", width = "1000px", height = "800px")),
+               tabPanel("AD", d3heatmapOutput("heat_AD", width = "1000px", height = "800px"))
+             ),
+             sliderInput('lambda', label = "Select Penalty", min = 0.001, max = 0.002, step = 0.0005, value = 0.001)),
+    tabPanel("Connectivity Difference"),
+    tabPanel("Connectivity Strength"),
+    
+    widths = c(3,9)
+  )
+))
