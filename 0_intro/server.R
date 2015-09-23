9abc6a8389b2aa3da7e5dcb9423ad3d757a778d1
Yan Jin <jyfeather@gmail.com>
2015-09-22 22:06:09 -0700

    update

--- 0_intro/server.R
+++ 0_intro/server.R
@@ -1,12 +1,56 @@
-# load preset data
-dataobs_AD <- read.csv(file = "MRI_AD.csv", header = T)
-dataobs_NL <- read.csv(file = "MRI_NL.csv", header = T)
-dataobs_MCI <- read.csv(file = "MRI_MCI.csv", header = T)
-nodes <- read.csv(file = "AAL_Yan.csv")
-rownames(dataobs_AD) <- nodes$name
+library(shiny)
+library(glasso)
+library(d3heatmap)
 
+# function definition
+matScale <- function(mat, diag = FALSE) {
+  mat <- abs(mat)
+  if (!diag) {
+    diag(mat) <- 0
+  }
+  mat <- apply(mat, MARGIN = 2, FUN = function(x) {(x - min(x))/diff(range(x))})
+  return(mat)
+}
+
+# for d3heatmap use
+reorderfun <- function(d, w) {
+  return(d)
+}
+
+# get dendrogram for reorder
+getDendrogram <- function(mat) {
+  distance <- dist(mat)
+  cluster <- hclust(distance, method = "ward.D")
+  dendrogram <- as.dendrogram(cluster)
+  Rowv <- rowMeans(mat)
+  dendrogram <- reorder(dendrogram, Rowv)
+  return(dendrogram)
+}
+
+# shiny server
 shinyServer(function(input, output) {
-  output$table <- renderTable({
-    t(dataobs_AD[1:6, 1:18])
+  # reactive data
+  dat <- reactive({
+    inFile <- input$fileAD
+    if (is.null(inFile)) {
+      return(NULL) 
+    }
+    tmpdat <- read.csv(inFile$datapath, header = input$header)
+    t(tmpdat)
   })
-})
+  
+  # tab1: data preview
+  output$table <- renderTable({
+    tmpdat <- dat()
+    tmpdat[1:10, 1:10]
+  })
+  
+  # tab2: heatmap
+  output$heat <- renderD3heatmap({
+    cov <- cov(dat())
+    fit <- glasso(s = cov, rho = input$lambda)
+    gh <- matScale(fit$wi)
+    dendrogram <- getDendrogram(gh)
+    d3heatmap(gh, Rowv = NA, dendrogram = 'none')
+  })
+}) # shinyServer end
--- 0_intro/ui.R
+++ 0_intro/ui.R
@@ -1,37 +1,28 @@
 library(shiny)
+library(d3heatmap)
 
 shinyUI(fluidPage(
   # first: upload data
   titlePanel("Introduction to Brain Connectivity"),
+  helpText('This app shows how penalty term controls the sparsity of network.'),
   sidebarLayout(
     sidebarPanel(
-      helpText('Note: A demo is shown at the right. Each row = each subject, while each column = each ROI region'),
-      fileInput('fileAD', 'Upload files for AD (Alzheimer)', accept = c('text/csv', '.csv')), 
-      actionButton('showAD', 'Quick look'),
+      fileInput('fileAD', 'Upload files for AD (Alzheimer\'s Disease)', accept = c('text/csv', '.csv')), 
+      checkboxInput('header', 'Header', TRUE),
+      helpText('Note: If you want a sample .csv to upload, you can first download the sample',
+               a(href='MRI_AD.csv', 'MRI_AD.csv'), ', and then try uploading it.'),
       tags$hr(),
-      fileInput('fileMCI', 'Upload files for MCI (Mild Cognitive Impairment)', accept = c('text/csv', '.csv')), 
-      actionButton('showMCI', 'Quick look'),
+      sliderInput('lambda', 'Choose Lambda:', min = 0, max = 0.01, value = 0.001),
+      sliderInput('threshold', 'Choose Threshold:', min = 0, max = 1, value = 0.8),
       tags$hr(),
-      fileInput('fileNL', 'Upload files for NL (Normal)', accept = c('text/csv', '.csv')),
-      actionButton('showNL', 'Quick look'),
-      tags$hr(),
-      actionButton('showResult', 'See Result')
-    ),
+      actionButton('update', 'update')
+    ), # end sidebarPanel
+    
     mainPanel(
-      tableOutput('table')
-    )
-  ),
-  tags$hr(),
-  
- # second: choose parameter
- absolutePanel(
-   bottom = 20, right = 20, width = 300, draggable = TRUE,
-   wellPanel(
-     sliderInput('lambda', 'Choose Penalty to Control Network Sparsity', min = 0, max = 0.1, value = 0.001),
-     style = "opacity: 0.9"
-   )
- ),
- 
- # third: compare two groups
- plotOutput('plot')
-))
+      tabsetPanel(
+        tabPanel('Data', tableOutput('table')),
+        tabPanel('Connectivity', d3heatmapOutput('heat'))
+      )
+    ) # end mainPanel
+  ) # end sidebarLayout
+)) # end shinyUI
